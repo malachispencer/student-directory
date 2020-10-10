360378049e3fccafca2b07462fa07d3b85a79992
Malachi Spencer <m.spencer@live.co.uk>
2020-10-10 17:23:34 +0200

    Refactored interactive_menu code into methods

--- directory.rb
+++ directory.rb
@@ -1,25 +1,9 @@
+@students = []
+
 def interactive_menu
-  students = []
-
   loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-
-    selection = gets.chomp
-
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print_students(students)
-      print_footer(students)
-    when "9"
-      exit
-    else
-      puts "I don't know what you mean, try again."
-    end
+    print_menu
+    process(gets.chomp)
   end
 end
 
@@ -27,15 +11,31 @@ def input_students
   puts "Please enter the name of the students"
   puts "To finish, just hit return twice"
   name = gets.chomp
-  students = []
-
+  
   while !name.empty?
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
+end
 
-  students
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def process(selection)
+  case selection
+  when "1"
+    students = input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you mean, try again."
+  end
 end
 
 def print_header
@@ -43,14 +43,20 @@ def print_header
   puts "-------------"
 end
 
-def print_students(students)
-  students.each do |student| 
+def print_students_list
+  @students.each do |student| 
     puts "#{student[:name]} (#{student[:cohort]})"
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
 end
 
 interactive_menu
