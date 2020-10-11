bff8e0d97de6dc6d014794a3bd77addef75fc840
Malachi Spencer <m.spencer@live.co.uk>
2020-10-11 10:42:34 +0200

    Added try_load_students method

--- directory.rb
+++ directory.rb
@@ -3,19 +3,7 @@
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
-  end
-end
-
-def input_students
-  puts "Please enter the name of the students"
-  puts "To finish, just hit return twice"
-  name = gets.chomp
-
-  while !name.empty?
-    @students << {name: name, cohort: :november}
-    puts "Now we have #{@students.count} students"
-    name = gets.chomp
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -44,6 +32,24 @@ def process(selection)
   end
 end
 
+def input_students
+  puts "Please enter the name of the students"
+  puts "To finish, just hit return twice"
+  name = STDIN.gets.chomp
+
+  while !name.empty?
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    name = gets.chomp
+  end
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
@@ -59,12 +65,6 @@ def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
 
-def show_students
-  print_header
-  print_students_list
-  print_footer
-end
-
 def save_students
   file = File.open("students.csv", "w")
   @students.each do |student|
@@ -75,8 +75,8 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(",")
     @students << {name: name, cohort: cohort.to_sym}
@@ -84,4 +84,17 @@ def load_students
   file.close
 end
 
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}."
+  else
+    puts "Sorry, #{filename} doesn't exist."
+    exit
+  end
+end
+
+try_load_students
 interactive_menu
